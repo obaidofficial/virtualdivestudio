698c99ee30bca9fbd95bf7b41e9bce16a53e18aa
Obaid Ullah <itsobaidofficial@gmail.com>
2026-03-09 13:41:05 +0500

    Social Networking And Ar Vr Entertainment Solutions Page added

---
+++ app/industries/social-networking-and-ar-vr-entertainment-solutions/components/Accordian/acoordianConstants.tsx
@@ -0,0 +1,68 @@
+import { AccordianItemData } from "../../../../../constants/accordianItemData";
+
+export const DEFAULT_ACCORDIAN_ITEMS: AccordianItemData[] = [
+  {
+    id: "1",
+    title:
+      "How can social platforms for the metaverse enhance social and entertainment experiences?",
+    content: (
+      <div>
+        <p>
+          Social platforms for the metaverse offer immersive experiences and
+          major networking opportunities. By developing these platforms with
+          Virtual Dive, you can provide users with a whole new level of social
+          interaction and entertainment in virtual environments, keeping your
+          brand at the forefront of digital innovation.
+        </p>
+      </div>
+    ),
+  },
+  {
+    id: "2",
+    title:
+      "What advantages do AR and VR integration bring to social platforms?",
+    content: (
+      <div>
+        <p>
+          Integrating AR and VR technologies into social platforms allows for
+          interactive experiences and fun AR filters for social posts. These
+          technologies can significantly enhance user engagement, attract more
+          users, and drive network growth by offering unique and immersive
+          features.
+        </p>
+      </div>
+    ),
+  },
+  {
+    id: "3",
+    title:
+      "How does Virtual Dive transform the social networking and entertainment industry with technology?",
+    content: (
+      <div>
+        <p>
+          Virtual Dive leverages cutting-edge technology to bring exciting
+          changes to the social networking and entertainment domains. By
+          offering innovative solutions such as custom platforms, AR/VR
+          integration, and live streaming services, Virtual Dive helps
+          businesses stay ahead in the digital era and enhance user experiences.
+        </p>
+      </div>
+    ),
+  },
+  {
+    id: "4",
+    title:
+      "Why should I choose Virtual Dive for my social networking and entertainment solutions?",
+    content: (
+      <div>
+        <p>
+          Clients choose Virtual Dive for our unbeatable pricing, transparent
+          communication, and dedication to delivering high-quality solutions.
+          Our expertise in social networking and entertainment technology,
+          combined with our commitment to customer satisfaction, makes us a
+          trusted partner for businesses seeking to innovate.
+        </p>
+      </div>
+    ),
+  },
+];
---
+++ app/industries/social-networking-and-ar-vr-entertainment-solutions/components/SocialNetworkingAppDevelopmentServices/SocialNetworkingAppDevelopmentServices.tsx
@@ -0,0 +1,132 @@
+import React from "react";
+import { PiInstagramLogo, PiVirtualReality } from "react-icons/pi";
+import { BsMeta, BsYoutube } from "react-icons/bs";
+import { PencilIcon } from "@heroicons/react/16/solid";
+import { VideoCameraIcon } from "@heroicons/react/24/outline";
+
+const SocialNetworkingAppDevelopmentServices = () => {
+  return (
+    <div className="bg-[#0c000a] pt-16 pb-16">
+      <div className="pt-6 w-[94%] lg:w-[90%] mx-auto pb-6">
+        <div className="grid grid-cols-1 lg:grid-cols-12 gap-2 lg:gap-4">
+          <div className="lg:col-span-12 py-2.5 px-4">
+            <h2 className="text-[28px] md:text-[32] text-center lg:text-[42px] text-white tracking-[0.5px] leading-[1.1em]">
+              Social Networking And Entertainment App Development Services
+            </h2>
+            <p className="text-white text-[16px] lg:text-[20px] font-light tracking-[0.17px] mt-4 text-center">
+              Immerse yourself in the future of connection and fun with our
+              social networking and entertainment services.
+            </p>
+          </div>
+
+          <div className="lg:col-span-4 py-8 px-8 flex flex-col border border-[#1e001b] hover:border-[#FF00E5] rounded-xl bg-linear-to-b from-[#1e001b] to-[#b500a3]">
+            <div className="px-2 pt-8 pb-6 text-center mx-auto">
+              <PiInstagramLogo className="w-15 h-15 text-white mx-auto" />
+              <div>
+                <h4 className="text-[18px] md:text-[22px] pt-3 text-[#ffffff] leading-[1.3em] tracking-[0.1px]">
+                  Custom Social Platforms
+                </h4>
+                <p className="text-[15px] font-light pt-2 text-white leading-[1.4em] tracking-[0.1px] px-4">
+                  In case you need platforms for sharing photos, videos, and
+                  updates like Facebook and Instagram or a professional
+                  networking platform for business professionals like LinkedIn
+                  then we can be at your service.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="lg:col-span-4 py-8 px-8 flex flex-col border border-[#1e001b] hover:border-[#FF00E5] rounded-xl bg-linear-to-b from-[#1e001b] to-[#b500a3]">
+            <div className="px-2 pt-8 pb-6 text-center mx-auto">
+              <BsMeta className="w-15 h-15 text-white mx-auto" />
+              <div>
+                <h4 className="text-[18px] md:text-[22px] pt-3 text-[#ffffff] leading-[1.3em] tracking-[0.1px]">
+                  Social Platforms for the Metaverse
+                </h4>
+                <p className="text-[15px] font-light pt-2 text-white leading-[1.4em] tracking-[0.1px] px-4">
+                  Metaverse is the new social hub and has great networking
+                  opportunities to give. Use these opportunities and have your
+                  social platforms created by Virtual Dive for a new social and
+                  entertainment experience!
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="lg:col-span-4 py-8 px-8 flex flex-col border border-[#1e001b] hover:border-[#FF00E5] rounded-xl bg-linear-to-b from-[#1e001b] to-[#b500a3]">
+            <div className="px-2 pt-8 pb-6 text-center mx-auto">
+              <PiVirtualReality className="w-15 h-15 text-white mx-auto" />
+              <div>
+                <h4 className="text-[18px] md:text-[22px] pt-3 text-[#ffffff] leading-[1.3em] tracking-[0.1px]">
+                  Convergence of AR/VR in Social Platforms
+                </h4>
+                <p className="text-[15px] font-light pt-2 text-white leading-[1.4em] tracking-[0.1px] px-4">
+                  The latest AR and VR technologies should be integrated into
+                  your social platforms so that the audience will have
+                  interactive experiences and fun AR filters for their social
+                  posts. These technologies could be the future of your network
+                  expansion.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="lg:col-span-4 py-8 px-8 flex flex-col border border-[#1e001b] hover:border-[#FF00E5] rounded-xl bg-linear-to-b from-[#1e001b] to-[#b500a3]">
+            <div className="px-2 pt-8 pb-6 text-center mx-auto">
+              <PencilIcon className="w-15 h-15 text-white mx-auto" />
+              <div>
+                <h4 className="text-[18px] md:text-[22px] pt-3 text-[#ffffff] leading-[1.3em] tracking-[0.1px]">
+                  Content Creating Platforms
+                </h4>
+                <p className="text-[15px] font-light pt-2 text-white leading-[1.4em] tracking-[0.1px] px-4">
+                  With us, you can reinvent YouTube and TikTok. Our services in
+                  content-creating platforms are outstanding, whether it is
+                  audio or video content on our platform you will get what you
+                  need.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="lg:col-span-4 py-8 px-8 flex flex-col border border-[#1e001b] hover:border-[#FF00E5] rounded-xl bg-linear-to-b from-[#1e001b] to-[#b500a3]">
+            <div className="px-2 pt-8 pb-6 text-center mx-auto">
+              <VideoCameraIcon className="w-15 h-15 text-white mx-auto" />
+              <div>
+                <h4 className="text-[18px] md:text-[22px] pt-3 text-[#ffffff] leading-[1.3em] tracking-[0.1px]">
+                  Live Streaming Social Networks
+                </h4>
+                <p className="text-[15px] font-light pt-2 text-white leading-[1.4em] tracking-[0.1px] px-4">
+                  Live streaming platforms have become popular as users want to
+                  interact and engage in real-time. Social networks can be used
+                  to organize live streaming sessions with friends or as a
+                  platform for official purposes, therefore it is a wise
+                  investment.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="lg:col-span-4 py-8 px-8 flex flex-col border border-[#1e001b] hover:border-[#FF00E5] rounded-xl bg-linear-to-b from-[#1e001b] to-[#b500a3]">
+            <div className="px-2 pt-8 pb-6 text-center mx-auto">
+              <BsYoutube className="w-15 h-15 text-white mx-auto" />
+              <div>
+                <h4 className="text-[18px] md:text-[22px] pt-3 text-[#ffffff] leading-[1.3em] tracking-[0.1px]">
+                  Entertainment Streaming Services
+                </h4>
+                <p className="text-[15px] font-light pt-2 text-white leading-[1.4em] tracking-[0.1px] px-4">
+                  The emergence of streaming services has completely changed the
+                  entertainment business. Such platforms as Netflix, Hulu,
+                  Amazon Prime Video, Spotify, and many others are giving users
+                  a simple way of streaming. Virtual Dive also enables you to
+                  create modern streaming platforms and maximize your ROI.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SocialNetworkingAppDevelopmentServices;
